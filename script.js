044369470fb138a143d4c693329f34e1cff6e81e
Ranjit Pote <116176451+Ranjitpote@users.noreply.github.com>
2023-01-08 21:53:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+
+    function updateLetterCount() {
+      // Get the text from the textbox
+      const text = document.getElementById('evaluatedText').value;
+
+      // Update the letter count
+      document.getElementById('letterCount').innerHTML = text.length;
+    }
+  
