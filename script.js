cc1d7044fec929eeb89d8cd7c4d31b0a58e3d319
Ranjit Pote <116176451+Ranjitpote@users.noreply.github.com>
2023-01-12 16:48:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your code here
 
-    function updateLetterCount() {
-      // Get the text from the textbox
-      const text = document.getElementById('evaluatedText').value;
+    let textArea = document.getElementById("evaluatedText");
+let h3 = document.getElementById("letterCount");
 
-      // Update the letter count
-      document.getElementById('letterCount').innerHTML = text.length;
-    }
-  
+textArea.addEventListener("keyup", () => {
+  func();
+});
+
+let func = () => {
+  console.log("helli");
+  let count = textArea.value.length;
+  h3.innerText = count;
+};
